917e17498c93c2d84e2b053e2c409ee463543769
Doesn'tmeananything <andrey.krasnov.pm@gmail.com>
2021-05-03 12:11:10 +0300

    Update dependencies

--- solutions/1-two-sum.test.ts
+++ solutions/1-two-sum.test.ts
@@ -1,7 +1,7 @@
 import { twoSum } from "./1-two-sum";
 
 describe("twoSum returns indices of the two numbers such that they add up to target", () => {
-  it("should return [0, 1] with nums [2,7,11,15] and target 9", () => {
+  it("should return [0, 1] with nums [2, 7, 11, 15] and target 9", () => {
     expect(twoSum([2, 7, 11, 15], 9)).toStrictEqual([0, 1]);
   });
 
